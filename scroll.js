fa9ba60d01fb5a7949cd19641d609d754ea25dca
Omar Nabil Mohamed Ahmed Omar <Onabil97971@yahoo.com>
2022-09-30 02:06:54 +0200

    last commit

--- scroll.js
+++ scroll.js
@@ -1,34 +1,35 @@
 document.lastScrollPosition = 0;
-document.lastCenteced = 0;
+document.lastCentered = 0;
 document.onWayTo = null;
 
-document.addEventListener('scroll', () => {
-    const direction = window.pageYOffset - document.lastScrollPosition > 0 ? 'down' : 'up';
-    const sections = [...document.querySelectorAll('section')];
+document.addEventListener("scroll", () => {
+  const direction =
+    window.pageYOffset - document.lastScrollPosition > 0 ? "down" : "up";
+  const sections = [...document.querySelectorAll("section")];
 
-
-    if (document.onWayTo === null) {
-        const destIndex = direction === 'up' ? document.lastCenteced - 1 : document.lastCenteced + 1;
-        if (destIndex >= 0 && destIndex < sections.length) {
-            document.onWayTo = destIndex;
-            window.scroll(0, sections[destIndex].offsetTop);
-        }
-    
+  if (document.onWayTo === null) {
+    const destIndex =
+      direction === "up"
+        ? document.lastCentered - 1
+        : document.lastCentered + 1;
+    if (destIndex >= 0 && destIndex < sections.length) {
+      console.log({ destIndex, direction });
+      document.onWayTo = destIndex;
+      window.scroll(0, sections[destIndex].offsetTop);
     }
-  
+  }
 
+  sections.forEach((section, index) => {
+    if (window.pageYOffset === section.offsetTop) {
+      document.lastCentered = index;
+      section.className = "active";
+      if (document.onWayTo === index) {
+        document.onWayTo = null;
+      }
+    } else {
+      section.className = "";
+    }
+  });
 
-    sections.forEach((section, index) => {
-        if (windows.pageYOffset === section.offsetTop) {
-            document.lastCenteced = index;
-            section.classNmae = 'active';
-            if (document.onWayTo === index) {
-                document.onWayTo = null;
-            }
-        } else {
-            section.className = '';
-        }
-    })
-   
-    document.lastScrollPosition = window.pageYOffset;
-})
+  document.lastScrollPosition = window.pageYOffset;
+});
